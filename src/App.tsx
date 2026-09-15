0bfd585942b5d249c6ebbcf3ce87667d5e22be10
mwafi8211 <mwafi8211@gmail.com>
2026-09-15 06:07:51 +0300

    ضبط مسافة سحب TrustBadges تحت الهيرو

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function HomePage() {
   return (
     <>
       <HeroSection />
-      <div className="-mt-[clamp(9rem,22vw,15rem)] relative z-10">
+      <div className="-mt-[clamp(3.2rem,10vw,5.5rem)] relative z-10">
         <TrustBadges />
       </div>
       <CategoriesSection />
